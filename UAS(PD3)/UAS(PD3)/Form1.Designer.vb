12e347db41d0b79d00db78d35ad51f567c7b8f4b
cellsior <mahesapramudyadarma@gmail.com>
2026-06-24 11:03:05 +0700

    Add files via upload

---
+++ UAS(PD3)/UAS(PD3)/Form1.Designer.vb
@@ -0,0 +1,262 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txtkodebarang = New System.Windows.Forms.TextBox()
+        Me.txtnamabarang = New System.Windows.Forms.TextBox()
+        Me.txthargajual = New System.Windows.Forms.TextBox()
+        Me.txtstok = New System.Windows.Forms.TextBox()
+        Me.txthargabeli = New System.Windows.Forms.TextBox()
+        Me.cbsatuan = New System.Windows.Forms.ComboBox()
+        Me.datagridviewbarang = New System.Windows.Forms.DataGridView()
+        Me.btnsimpan = New System.Windows.Forms.Button()
+        Me.btnhapus = New System.Windows.Forms.Button()
+        Me.btnedit = New System.Windows.Forms.Button()
+        Me.btnkeluar = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        CType(Me.datagridviewbarang, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(43, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(316, 31)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ENTRI DATA BARANG"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(46, 114)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(102, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Kode Barang"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(46, 150)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(107, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Nama Barang"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(46, 186)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(61, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Satuan"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(46, 229)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(83, 20)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Harga Beli"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(46, 270)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(86, 20)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Harga Jual"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(46, 309)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(42, 20)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Stok"
+        '
+        'txtkodebarang
+        '
+        Me.txtkodebarang.Location = New System.Drawing.Point(234, 114)
+        Me.txtkodebarang.Name = "txtkodebarang"
+        Me.txtkodebarang.Size = New System.Drawing.Size(100, 20)
+        Me.txtkodebarang.TabIndex = 7
+        '
+        'txtnamabarang
+        '
+        Me.txtnamabarang.Location = New System.Drawing.Point(234, 150)
+        Me.txtnamabarang.Name = "txtnamabarang"
+        Me.txtnamabarang.Size = New System.Drawing.Size(157, 20)
+        Me.txtnamabarang.TabIndex = 8
+        '
+        'txthargajual
+        '
+        Me.txthargajual.Location = New System.Drawing.Point(234, 270)
+        Me.txthargajual.Name = "txthargajual"
+        Me.txthargajual.Size = New System.Drawing.Size(157, 20)
+        Me.txthargajual.TabIndex = 9
+        '
+        'txtstok
+        '
+        Me.txtstok.Location = New System.Drawing.Point(234, 309)
+        Me.txtstok.Name = "txtstok"
+        Me.txtstok.Size = New System.Drawing.Size(100, 20)
+        Me.txtstok.TabIndex = 10
+        '
+        'txthargabeli
+        '
+        Me.txthargabeli.Location = New System.Drawing.Point(235, 229)
+        Me.txthargabeli.Name = "txthargabeli"
+        Me.txthargabeli.Size = New System.Drawing.Size(156, 20)
+        Me.txthargabeli.TabIndex = 11
+        '
+        'cbsatuan
+        '
+        Me.cbsatuan.FormattingEnabled = True
+        Me.cbsatuan.Items.AddRange(New Object() {"Pcs", "Box", "Pack", "Liter"})
+        Me.cbsatuan.Location = New System.Drawing.Point(234, 188)
+        Me.cbsatuan.Name = "cbsatuan"
+        Me.cbsatuan.Size = New System.Drawing.Size(157, 21)
+        Me.cbsatuan.TabIndex = 12
+        '
+        'datagridviewbarang
+        '
+        Me.datagridviewbarang.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.datagridviewbarang.Location = New System.Drawing.Point(426, 186)
+        Me.datagridviewbarang.Name = "datagridviewbarang"
+        Me.datagridviewbarang.Size = New System.Drawing.Size(595, 332)
+        Me.datagridviewbarang.TabIndex = 13
+        '
+        'btnsimpan
+        '
+        Me.btnsimpan.Location = New System.Drawing.Point(508, 110)
+        Me.btnsimpan.Name = "btnsimpan"
+        Me.btnsimpan.Size = New System.Drawing.Size(75, 23)
+        Me.btnsimpan.TabIndex = 14
+        Me.btnsimpan.Text = "Simpan"
+        Me.btnsimpan.UseVisualStyleBackColor = True
+        '
+        'btnhapus
+        '
+        Me.btnhapus.Location = New System.Drawing.Point(636, 110)
+        Me.btnhapus.Name = "btnhapus"
+        Me.btnhapus.Size = New System.Drawing.Size(75, 23)
+        Me.btnhapus.TabIndex = 15
+        Me.btnhapus.Text = "Hapus"
+        Me.btnhapus.UseVisualStyleBackColor = True
+        '
+        'btnedit
+        '
+        Me.btnedit.Location = New System.Drawing.Point(763, 110)
+        Me.btnedit.Name = "btnedit"
+        Me.btnedit.Size = New System.Drawing.Size(75, 23)
+        Me.btnedit.TabIndex = 16
+        Me.btnedit.Text = "Edit"
+        Me.btnedit.UseVisualStyleBackColor = True
+        '
+        'btnkeluar
+        '
+        Me.btnkeluar.Location = New System.Drawing.Point(888, 110)
+        Me.btnkeluar.Name = "btnkeluar"
+        Me.btnkeluar.Size = New System.Drawing.Size(75, 23)
+        Me.btnkeluar.TabIndex = 17
+        Me.btnkeluar.Text = "Keluar"
+        Me.btnkeluar.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(234, 114)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1033, 530)
+        Me.Controls.Add(Me.btnkeluar)
+        Me.Controls.Add(Me.btnedit)
+        Me.Controls.Add(Me.btnhapus)
+        Me.Controls.Add(Me.btnsimpan)
+        Me.Controls.Add(Me.datagridviewbarang)
+        Me.Controls.Add(Me.cbsatuan)
+        Me.Controls.Add(Me.txthargabeli)
+        Me.Controls.Add(Me.txtstok)
+        Me.Controls.Add(Me.txthargajual)
+        Me.Controls.Add(Me.txtnamabarang)
+        Me.Controls.Add(Me.txtkodebarang)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.datagridviewbarang, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents txtkodebarang As System.Windows.Forms.TextBox
+    Friend WithEvents txtnamabarang As System.Windows.Forms.TextBox
+    Friend WithEvents txthargajual As System.Windows.Forms.TextBox
+    Friend WithEvents txtstok As System.Windows.Forms.TextBox
+    Friend WithEvents txthargabeli As System.Windows.Forms.TextBox
+    Friend WithEvents cbsatuan As System.Windows.Forms.ComboBox
+    Friend WithEvents datagridviewbarang As System.Windows.Forms.DataGridView
+    Friend WithEvents btnsimpan As System.Windows.Forms.Button
+    Friend WithEvents btnhapus As System.Windows.Forms.Button
+    Friend WithEvents btnedit As System.Windows.Forms.Button
+    Friend WithEvents btnkeluar As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+
+End Class
---
+++ UAS(PD3)/UAS(PD3)/Form1.vb
@@ -0,0 +1,109 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Dim koneksi As SqlConnection
+    Dim cmd As SqlCommand
+    Dim da As SqlDataAdapter
+    Dim ds As DataSet
+
+    Sub konek()
+        koneksi = New SqlConnection("Data Source=LAPTOP-N8MU1NND;Initial Catalog=DBUASPD3;Integrated Security=True")
+        If koneksi.State = ConnectionState.Closed Then
+            koneksi.Open()
+        End If
+    End Sub
+
+
+    Sub tampil()
+        konek()
+        da = New SqlDataAdapter("SELECT * FROM tbbarang", koneksi)
+        ds = New DataSet
+        da.Fill(ds, "tbbarang")
+        datagridviewbarang.DataSource = ds.Tables("tbbarang")
+    End Sub
+
+    Sub bersih()
+        txtkodebarang.Clear()
+        txtnamabarang.Clear()
+        txthargabeli.Clear()
+        txthargajual.Clear()
+        txtstok.Clear()
+        cbsatuan.Text = ""
+        txtkodebarang.Focus()
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        tampil()
+        bersih()
+    End Sub
+
+    ' ================= SIMPAN =================
+    Private Sub btnsimpan_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnsimpan.Click
+        konek()
+        cmd = New SqlCommand("INSERT INTO tbbarang (kodebarang,namabarang,satuan,hargabeli,hargajual,stok) VALUES (@kode,@nama,@satuan,@hb,@hj,@stok)", koneksi)
+
+        cmd.Parameters.AddWithValue("@kode", txtkodebarang.Text)
+        cmd.Parameters.AddWithValue("@nama", txtnamabarang.Text)
+        cmd.Parameters.AddWithValue("@satuan", cbsatuan.Text)
+        cmd.Parameters.AddWithValue("@hb", Val(txthargabeli.Text))
+        cmd.Parameters.AddWithValue("@hj", Val(txthargajual.Text))
+        cmd.Parameters.AddWithValue("@stok", Val(txtstok.Text))
+
+        cmd.ExecuteNonQuery()
+        MessageBox.Show("Data berhasil disimpan")
+        tampil()
+        bersih()
+    End Sub
+
+    ' ================= EDIT =================
+    Private Sub btnedit_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnedit.Click
+        konek()
+        cmd = New SqlCommand("UPDATE tbbarang SET namabarang=@nama,satuan=@satuan,hargabeli=@hb,hargajual=@hj,stok=@stok WHERE kodebarang=@kode", koneksi)
+
+        cmd.Parameters.AddWithValue("@kode", txtkodebarang.Text)
+        cmd.Parameters.AddWithValue("@nama", txtnamabarang.Text)
+        cmd.Parameters.AddWithValue("@satuan", cbsatuan.Text)
+        cmd.Parameters.AddWithValue("@hb", Val(txthargabeli.Text))
+        cmd.Parameters.AddWithValue("@hj", Val(txthargajual.Text))
+        cmd.Parameters.AddWithValue("@stok", Val(txtstok.Text))
+
+        cmd.ExecuteNonQuery()
+        MessageBox.Show("Data berhasil diupdate")
+        tampil()
+        bersih()
+    End Sub
+
+    ' ================= HAPUS =================
+    Private Sub btnhapus_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnhapus.Click
+        If MessageBox.Show("Yakin ingin menghapus data?", "Konfirmasi",
+                           MessageBoxButtons.YesNo) = DialogResult.Yes Then
+            konek()
+            cmd = New SqlCommand("DELETE FROM tbbarang WHERE kodebarang=@kode", koneksi)
+            cmd.Parameters.AddWithValue("@kode", txtkodebarang.Text)
+            cmd.ExecuteNonQuery()
+
+            MessageBox.Show("Data berhasil dihapus")
+            tampil()
+            bersih()
+        End If
+    End Sub
+
+    ' ================= KELUAR =================
+    Private Sub btnkeluar_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnkeluar.Click
+        Me.Close()
+    End Sub
+
+    ' ================= KLIK GRID =================
+    Private Sub datagridviewbarang_CellClick(ByVal sender As Object, ByVal e As DataGridViewCellEventArgs) Handles datagridviewbarang.CellClick
+        If e.RowIndex >= 0 Then
+            txtkodebarang.Text = datagridviewbarang.Rows(e.RowIndex).Cells(0).Value
+            txtnamabarang.Text = datagridviewbarang.Rows(e.RowIndex).Cells(1).Value
+            cbsatuan.Text = datagridviewbarang.Rows(e.RowIndex).Cells(2).Value
+            txthargabeli.Text = datagridviewbarang.Rows(e.RowIndex).Cells(3).Value
+            txthargajual.Text = datagridviewbarang.Rows(e.RowIndex).Cells(4).Value
+            txtstok.Text = datagridviewbarang.Rows(e.RowIndex).Cells(5).Value
+        End If
+    End Sub
+
+End Class
